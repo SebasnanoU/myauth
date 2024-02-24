2050e0055d0b4fdd61df405e53ca72767872707f
Sebastian Rodriguez <105129159+SebasnanoU@users.noreply.github.com>
2024-02-24 13:09:04 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 # assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
-path = '/home/furiahispana/myauth'
+path = '/home/furiahispana/myauth/settings/base.py'
 if path not in sys.path:
     sys.path.insert(0, path)
 
